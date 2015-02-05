3f0b8870e06b61d26b2e1a6e0800468d8149fed7
Kendal Harland <kharland2011@gmail.com>
2015-02-05 17:54:35 -0600

    corrected plugin module name

--- dev-python/update-ccb.py
+++ dev-python/update-ccb.py
@@ -2,13 +2,13 @@
 
 import sys
 import ConfigParser
-import plugins.aid2gigeneid
+import plugins.Aid2GiGeneidAccessionUniprot
 
 if len(sys.argv) < 3:
   print "Usage: %s <username> <password>" % __file__
   sys.exit()
+user, passwd = sys.argv[1], sys.argv[2]
 
 cfg = ConfigParser.ConfigParser()
 cfg.read("config.cfg")
-user, passwd = sys.argv[1], sys.argv[2]
-plugins.aid2gigeneid.update(user, passwd, cfg.get('default','db'))
+plugins.Aid2GiGeneidAccessionUniprot.update(user, passwd, cfg.get('default','db'))
