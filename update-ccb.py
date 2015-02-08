71821c563a0cd4056c629fc08bf095b75e0786c9
Kendal Harland <kharland2011@gmail.com>
2015-02-07 23:55:57 -0600

    modified plugin names in update-ccb to reflect new files

--- update-ccb.py
+++ update-ccb.py
@@ -3,14 +3,15 @@
 import sys
 import ConfigParser
 import plugins.Aid2GiGeneidAccessionUniprot
-import plugins.BioassayResults
+import plugins.Bioassays
+import plugins.Compounds
 
 if len(sys.argv) < 3:
   print "Usage: %s <username> <password>" % __file__
   sys.exit()
 user, passwd = sys.argv[1], sys.argv[2]
-
 cfg = ConfigParser.ConfigParser()
 cfg.read("config.cfg")
-plugins.Aid2GiGeneidAccessionUniprot.update(user, passwd, cfg.get('default','db'))
-plugins.BioassayResults.update(user, passwd, cfg.get('default', 'db'))
+#plugins.Bioassays.update(user, passwd, cfg.get('default', 'db'))
+plugins.Compounds.update(user, passwd, cfg.get('default', 'db'))
+#plugins.Aid2GiGeneidAccessionUniprot.update(user, passwd, cfg.get('default','db'))
