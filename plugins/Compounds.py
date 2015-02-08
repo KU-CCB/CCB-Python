f470465fb38c32b35316de759d9a0917ac85a80b
Kendal Harland <kharland2011@gmail.com>
2015-02-07 23:56:41 -0600

    added 'store'

---
+++ plugins/Compounds.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+
+import sys
+import os
+import subprocess
+from ftplib import FTP
+import gzip 
+from datetime import datetime, timedelta
+import mysql.connector
+from mysql.connector import errorcode
+from mysql.connector.constants import ClientFlag
+
+TABLE_NAME = "Compounds"
+server = "ftp.ncbi.nih.gov"
+pubchemDir  = "pubchem/Compound/Weekly"
+foundUpdated = False
+localDir = "./data/compounds"
+localFiles = ["smiles.csv", "chemattr.csv"]
+
+def _checkForUpdates(folder):
+  global pubchemDir
+  global foundUpdated
+  folder = folder.split()
+  lastUpdated, name = folder[-1], folder[-1]
+  sevenDaysAgo = (datetime.today() - timedelta(days=7)).strftime("%Y-%m-%d")
+  if sevenDaysAgo < lastUpdated:
+    pubchemDir = "%s/SDF" % name
+    foundUpdated = True
+
+def update(user, passwd, db):
+  print "plugin: %s" % __name__
+  print "> creating space on local machine"
+  if not os.path.exists(localDir):
+    os.makedirs(localDir)
+  print "> building client binaries"
+  make_process = subprocess.Popen("make")
+  if make_process.wait() != 0:
+    print "x failed to building client binaries. Check ./plugins/store/makefile for errors."
+  print "> checking for updated files"
+  ftp = FTP(server)
+  ftp.login() # anonymous
+  ftp.cwd(pubchemDir)
+  ftp.retrlines("LIST", _checkForUpdates)
+  if foundUpdated:
+    print "> downloading files %s " % pubchemDir
+    ftp.cwd(pubchemDir)
+    for pubchemFile in ftp.nlst():
+      print pubchemFile
+      localFile = "%s/%s" % (localDir, pubchemFile)
+      ftp.retrbinary("RETR %s" % pubchemFile, open("%s" % localFile, 'wb').write)
+  ftp.quit()
+  sys.exit()
+  # Fix below
+  print "> extracting files"
+  with gzip.open("%s/data/%s" % (os.getcwd(), localFile), 'rb') as inf:
+    with open("%s/data/%s" % (os.getcwd(), localFile), 'w') as outf:
+      for line in inf:
+        outf.write(line)
+  print "> loading %s into table" % localFile
+  cnx = mysql.connector.connect(user=user, passwd=passwd, db=db, client_flags=[ClientFlag.LOCAL_FILES])
+  cursor = cnx.cursor()
+  try:
+    cursor.execute(
+        "LOAD DATA LOCAL INFILE '%s/data/%s'"
+        " REPLACE"
+        " INTO TABLE %s"
+        " FIELDS TERMINATED BY '\t'"
+        " LINES TERMINATED BY  '\n'"
+        " IGNORE 1 LINES ("
+        " AID,"
+        " GI,"
+        " GeneID,"
+        " Accession,"
+        " UniProtKB_ACID);"  % 
+        (os.getcwd(), localFile, TABLE_NAME))
+  except mysql.connector.Error as e:
+    sys.stderr.write("x Failed loading data: {}\n".format(e))
+    return
+  print "> %s complete" % __name__
+
+if __name__=="__main__":
+  if len(sys.argv) < 4:
+    print "Usage: %s <username> <password> <database>" % __file__
+    sys.exit()
+  update(sys.argv[1], sys.argv[2], sys.argv[3])
