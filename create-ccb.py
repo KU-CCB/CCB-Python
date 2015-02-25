78a8e5ef4ac73b2f9dd005fad36786e8afbad001
Kendal Harland <kharland2011@gmail.com>
2015-02-25 09:14:59 -0600

    added new fields to indicate author info and repository locations

--- create-ccb.py
+++ create-ccb.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
+"""
+TODO
+- Move column types into config file
+"""
 
 import sys
 
-"""
-mysq.connector is only installed on the acf cluster for python2.6 but we need
-to use python2.7+ because the 'with' directive is not backwards compatible
-with v2.6.*
-"""
 import socket
+# mysql.connector is only installed on the acf cluster for python2.6 but we need
+# to use python2.7+ because the 'with' directive is not backwards compatible
+# with v2.6.*
 if socket.gethostname()[-6:] == "ku.edu":
 	sys.path.append('/usr/lib/python2.6/site-packages/')
 import ConfigParser
@@ -27,95 +29,75 @@ if len(sys.argv) < 3:
 cfg = ConfigParser.ConfigParser()
 cfg.read("config.cfg")
 
+# s for each of the table columns restricted to a specific set of values 
+# default (assay|substance|compound)_id to 0 if missing
+TYPES = {
+  'assay_id':          "MEDIUMINT UNSIGNED DEFAULT 0",
+  'substance_id':      "INT UNSIGNED DEFAULT 0",
+  'compound_id':       "INT UNSIGNED DEFAULT 0",
+  'gene_id':           "INT UNSIGNED", 
+  'gi':                "INT UNSIGNED", # Global identifier
+  'ncbi_accession':    "varchar(15)", # Needs check (NCBI Accession)
+  'uniprot_kb':        "varchar(15)", # Needs check (Uniprot KB Accession/Identifier)
+  'activity_outcome':  "ENUM('Inactive','Active','Inconclusive','Unspecified','Probe')",
+  'activity_score':    "SMALLINT", # Needs check, encountered values in range (-128, 128) could be tinyint
+  'activity_URL':      "TEXT",
+  'assay_description': "MEDIUMTEXT", # Needs check, might have to be larger depending on description format (JSON, ASNX, etc.)
+}
+
 TABLES = {
   'Aid2GiGeneidAccessionUniprot': (
     "CREATE TABLE `Aid2GiGeneidAccessionUniprot` ("
-    "`AID`            bigint(20) NOT NULL,"
-    "`GI`             int(11) NOT NULL,"
-    "`GeneID`         int(11) NOT NULL,"
-    "`Accession`      varchar(15) DEFAULT NULL,"
-    "`UniProtKB_ACID` varchar(15) DEFAULT NULL,"
-    "PRIMARY KEY (`AID`,`GI`,`GeneID`),"
-    "KEY `geneid_index` (`GeneID`)"
+    "`assay_id`       " + TYPES["assay_id"] + " NOT NULL,"
+    "`gi`             " + TYPES["gi"] + " NOT NULL,"
+    "`gene_id`        " + TYPES["gene_id"] + ","
+    "`ncbi_accession` " + TYPES["ncbi_accession"] + ","
+    "`uniprot_kb`     " + TYPES["uniprot_kb"] + ","
+    "PRIMARY KEY (`assay_id`,`gi`,`gene_id`)" # WTF IS THIS SUPPOSED TO BE?
     ") ENGINE=MyISAM DEFAULT CHARSET=latin1;"
   ),
 
   'Bioassays': (
     "CREATE TABLE `Bioassays` ("
-    "`AID` int(15)       NOT NULL DEFAULT '0',"
-    "`SID` int(15)       NOT NULL DEFAULT '0',"
-    "`CID` int(15)       NOT NULL DEFAULT '0',"
-    "`Activity_Outcome`  varchar(15) DEFAULT NULL,"
-    "`Activity_Score`    tinyint(4) DEFAULT NULL,"
-    "`Activity_URL`      text DEFAULT NULL,"
-    "`Comment`           text,"
-    "PRIMARY KEY (`SID`,`CID`,`AID`),"
-    "KEY `cid_aid_idx` (`CID`,`AID`),"
-    "KEY `sid_aid_idx` (`CID`,`AID`),"
-    "KEY `aid_idx` (`AID`),"
-    "KEY `activity_outcome_idx` (`Activity_Outcome`)"
-    ") ENGINE=MyISAM AUTO_INCREMENT=227601852 DEFAULT CHARSET=latin1 ROW_FORMAT=FIXED"
+    "`assay_id`          " + TYPES["assay_id"] + ","
+    "`activity_outcome`  " + TYPES["activity_outcome"] + ","
+    "`activity_score`    " + TYPES["activity_score"] + ","
+    "`activity_URL`      " + TYPES["activity_URL"] + ","
+    "`assay_description` " + TYPES["assay_description"] + ","
+    "PRIMARY KEY (`assay_id`)"
+    ") ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1 ROW_FORMAT=FIXED"
   ),
 
-  'Compounds': (
-    "CREATE TABLE `Compounds` ("
-    " `PUBCHEM_COMPOUND_CID`              int(11) NOT  NULL,"
-    " `PUBCHEM_COMPOUND_CANONICALIZED`    int,"
-    " `PUBCHEM_CACTVS_COMPLEXITY`         int,"
-    " `PUBCHEM_CACTVS_HBOND_ACCEPTOR`     int,"
-    " `PUBCHEM_CACTVS_HBOND_DONOR`        int,"
-    " `PUBCHEM_CACTVS_ROTATABLE_BOND`     int,"
-    " `PUBCHEM_CACTVS_SUBSKEYS`           text,"
-    " `PUBCHEM_IUPAC_INCHI`               text,"
-    " `PUBCHEM_IUPAC_INCHIKEY`            text,"
-    " `PUBCHEM_EXACT_MASS`                float,"
-    " `PUBCHEM_MOLECULAR_FORMULA`         text,"
-    " `PUBCHEM_MOLECULAR_WEIGHT`          float,"
-    " `PUBCHEM_OPENEYE_CAN_SMILES`        text,"
-    " `PUBCHEM_OPENEYE_ISO_SMILES`        text,"
-    " `PUBCHEM_CACTVS_TPSA`               float,"
-    " `PUBCHEM_MONOISOTOPIC_WEIGHT`       float,"
-    " `PUBCHEM_TOTAL_CHARGE`              int,"
-    " `PUBCHEM_HEAVY_ATOM_COUNT`          int,"
-    " `PUBCHEM_ATOM_DEF_STEREO_COUNT`     int,"
-    " `PUBCHEM_ATOM_UDEF_STEREO_COUNT`    int,"
-    " `PUBCHEM_BOND_DEF_STEREO_COUNT`     int,"
-    " `PUBCHEM_BOND_UDEF_STEREO_COUNT`    int,"
-    " `PUBCHEM_ISOTOPIC_ATOM_COUNT`       int,"
-    " `PUBCHEM_COMPONENT_COUNT`           int,"
-    " `PUBCHEM_CACTVS_TAUTO_COUNT`        int,"
-    " PRIMARY KEY (`PUBCHEM_COMPOUND_CID`),"
-    " UNIQUE KEY `PUBCHEM_COMPOUND_CID` (`PUBCHEM_COMPOUND_CID`)"
-    ") ENGINE=InnoDB DEFAULT CHARSET=latin1"
-  ),
-
-  'ProteinTargets': (
-    "CREATE TABLE `ProteinTargets` ("
-    "`GI`             int(11) NOT NULL,"
-    "`FASTASequence`  text,"
-    "`dbName`         varchar(5) DEFAULT NULL,"
-    "`dbID`           varchar(20) DEFAULT NULL,"
-    "PRIMARY KEY (`GI`),"
-    "UNIQUE KEY `GeneID` (`GI`)"
-    ") ENGINE=MyISAM DEFAULT CHARSET=latin1"
+  'substance_id_compound_id': (
+    "CREATE TABLE `substance_id_compound_id` ("
+    "`substance_id` " + TYPES["substance_id"] + ","
+    "`compound_id`  " + TYPES["compound_id"] + ","
+    "PRIMARY KEY (`substance_id`),"
+    "KEY compound_id_idx (`compound_id`)"
+    ") ENGINE=MyISAM DEFAULT CHARSET=latin1;"
   )
 }
 
 cnx = mysql.connector.connect(user=sys.argv[1], password=sys.argv[2])
 cursor = cnx.cursor()
 
-# Create the database
-try:
-  cursor.execute(
-    "CREATE DATABASE %s DEFAULT CHARACTER SET '%s'" % 
-      (cfg.get('default','database'), cfg.get('default','charset')))
+try: # Create the database
+  cursor.execute("CREATE DATABASE %s DEFAULT CHARACTER SET '%s'" % 
+    (cfg.get('default','database'), cfg.get('default','charset')))
+  cursor.execute("use %s;" % cfg.get('default','database'))
   print "> Database %s successfully created" % cfg.get('default','database')
 except mysql.connector.Error as e:
-  sys.stderr.write("x Failed creating database: {}\n".format(e))
+  sys.stderr.write("x Failed creating database: %s\n" % e)
   sys.exit()
 
-# Access the databse
-try:
+try: # Set database options
+  cursor.execute("set sql_mode='STRICT_ALL_TABLES';") # Make mysql respect our domains
+  print "> Database %s configuration set successfully"
+except mysql.connector.Error as e:
+  sys.stderr.write("x Failed configuring database: %s\n" % e)
+  sys.exit()
+
+try: # Access the databse
   cursor.execute("use %s" % cfg.get('default','database'))
 except mysql.connector.Error as e:
   sys.stderr.write("x Failed to access database: %s\n" % e)
