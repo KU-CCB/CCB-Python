eae94422297a1685f75bbf1c1330f3c4a36a47de
Kendal Harland <kharland2011@gmail.com>
2015-02-25 09:16:16 -0600

    added comments for testing

--- update-ccb.py
+++ update-ccb.py
@@ -12,21 +12,25 @@ if socket.gethostname()[-6:] == "ku.edu":
 import ConfigParser
 import plugins.Aid2GiGeneidAccessionUniprot
 import plugins.Bioassays
-import plugins.Compounds
+import subprocess
 
-if len(sys.argv) < 3:
-  print "CCBDB.update-ccb  The KU CCB Database update script"
-  print "  For more information, see https://github.com/KU-CCB/CCBDB.git"
-  print ""
-  print "  For issues see https://github.com/KU-CCB/CCBDB/issues or"
-  print "  contact Kendal Harland <kendaljharland@gmail.com>"
-  print ""
-  print "  Usage: %s <username> <password>" % __file__
-  sys.exit()
-user, passwd = sys.argv[1], sys.argv[2]
 cfg = ConfigParser.ConfigParser()
 cfg.read("config.cfg")
 
-plugins.Aid2GiGeneidAccessionUniprot.update(user, passwd, cfg.get('default','database'))
+if len(sys.argv) < 3:
+  print "---------------------------------------------------------"
+  print "%s - The %s database update script" % (__file__, cfg.get('default', 'database'))
+  print "---------------------------------------------------------"
+  print "* For information, see %s." % (cfg.get('repo', 'url'))
+  print "* Report issues at %s or " % cfg.get('repo', 'issues')
+  print "* contact %s at <%s>" % (cfg.get('author', 'name'), cfg.get('author', 'email'))
+  print "Usage: %s <username> <password>" % __file__
+  print ""
+  sys.exit()
+user, passwd = sys.argv[1], sys.argv[2]
+
+# Create database backup with mysqldump
+
+# Run table update scripts
+#plugins.Aid2GiGeneidAccessionUniprot.update(user, passwd, cfg.get('default','database'))
 plugins.Bioassays.update(user, passwd, cfg.get('default', 'database'))
-plugins.Compounds.update(user, passwd, cfg.get('default', 'database'))
