bfbfc51866ff9acfcf51e2b37825dc967b0eda34
kharland <kharland2011@gmail.com>
2015-03-04 18:02:54 -0600

    refactored code and added comments

--- plugins/Bioassays.py
+++ plugins/Bioassays.py
@@ -15,131 +15,138 @@ __all__ = ["update"]
 plugin = __name__[__name__.index('.')+1:] if __name__ != "__main__"  else "main"
 cfg = ConfigParser.ConfigParser()
 cfg.read("config.cfg")
-server            = "ftp.ncbi.nih.gov"
-pubchemDir        = "pubchem/Bioassay/Concise/CSV/Data"
-assayDataDir      = "%s/assays"      % cfg.get('default','tmp')
-substanceDataDir  = "%s/substances"  % cfg.get('default','tmp')
-localZippedDir    = "%s/zipped"      % assayDataDir
-localUnzippedDir  = "%s/unzipped"    % assayDataDir
-localUngzippedDir = "%s/ungzipped"   % assayDataDir
-localProcessedDir = "%s/processed"   % assayDataDir
-assayDataFile     = "%s/assays.csv"  % localProcessedDir
-sid2cidMapFile    = "%s/sid2cid.csv" % substanceDataDir
+server          = "ftp.ncbi.nih.gov"
+pubchemURL      = "pubchem/Bioassay/Concise/CSV/Data"
+assayFolder     = "%s/assays"      % cfg.get('default','tmp')
+substanceFolder = "%s/substances"  % cfg.get('default','tmp')
+zippedFolder    = "%s/zipped"      % assayFolder
+unzippedFolder  = "%s/unzipped"    % assayFolder
+ungzippedFolder = "%s/ungzipped"   % assayFolder
+assayFolderFile = "%s/assays.csv"  % ungzippedFolder
+sid2cidMapFile  = "%s/sid2cid.csv" % substanceFolder
+
 
 def makedirs(dirs):
   for d in dirs:
     if not os.path.exists(d):
       os.makedirs(d)
 
+
 def downloadFiles():
   ftp = FTP(server)
   ftp.login() # anonymous
-  ftp.cwd(pubchemDir)
+  ftp.cwd(pubchemURL)
   files = ftp.nlst()
   for i in range(0, len(files)):
     sys.stdout.write("\r> downloading files (%04d/%04d)" % (i+1, len(files)))
     sys.stdout.flush()
-    ftp.retrbinary("RETR %s" % files[i], open("%s/%s" % (localZippedDir, files[i]), 'wb').write)
+    ftp.retrbinary("RETR %s" % files[i], open("%s/%s" % (zippedFolder, files[i]), 'wb').write)
   sys.stdout.write('\n')
   ftp.quit()
 
+
 def unzipFiles():
-  root,_,files = next(os.walk(localZippedDir))
+  root,_,files = next(os.walk(zippedFolder))
   for i in range(0, len(files)):
     sys.stdout.write("\r> unzipping files (%04d/%04d)" % (i+1, len(files)))
     sys.stdout.flush()
     archive = zipfile.ZipFile(os.path.join(root, files[i]), 'r')
-    archive.extractall(localUnzippedDir)
+    archive.extractall(unzippedFolder)
   sys.stdout.write("\n")
 
-def splitDataFiles():
-  fileNo = 0
-  root,folders,_ = next(os.walk(localUnzippedDir))
-  # Iterate over each folder containing gzipped files
-  for j in range(0, len(folders)):
-    _,_,gzfiles = next(os.walk(os.path.join(root, folders[j])))
-    # Iterate over each gzipped file in the folder
-    for i in range(0, len(gzfiles)):
-      sys.stdout.write("\r> splitting folder (%04d/%04d) files (%04d/%04d)" %
-        (j+1, len(folders), i+1, len(gzfiles)))
-      aid = gzfiles[i][:gzfiles[i].index('.')]
+
+def ungzipFiles():
+  root,folders,_ = next(os.walk(unzippedFolder))
+  for i in range(0, len(folders)):
+    _,_,gzfiles = next(os.walk(os.path.join(root, folders[i])))
+    for j in range(0, len(gzfiles)):
+      sys.stdout.write("\r> ungzipping folder (%04d/%04d) file (%04d/%04d)" % 
+        (i+1, len(folders), j+1, len(gzfiles)))
+      sys.stdout.flush()
+
+      aid = gzfiles[j][:gzfiles[j].index('.')]
       sid2cidData, assayData = [], []
-      with gzip.open(os.path.join(root, folders[j], gzfiles[i]), 'rb') as inf:
+      with gzip.open(os.path.join(root, folders[i], gzfiles[j]), 'rb') as inf:
         inf.readline()
         for line in inf:
           line = line.rstrip().split(',')
-          # - Index:        0    2        3      4    5
-          # - Keep the aid, sid, outcome, score, url, comment
-          # - Discard everything after column 7 (active concentration and data
+          # * Index:        0    2        3      4    5
+          # * Keep the aid, sid, outcome, score, url, comment
+          # * Discard everything after column 7 (active concentration and data
           #   specified at ftp://ftp.ncbi.nih.gov/pubchem/Bioassay/Concise/CSV/README)```
           assayData.append([aid, line[0], line[2], line[3], line[4], line[5]])
-          # Now store the sid/cid map data
-          #            0       1
-          # - Keep the sid and cid
+          # * Index:   0       1
+          # * Keep the sid and cid
+          # * We have to store the sid and cid data from these files in a separate database
+          #   table, so we'll write the file here and pass it off to the next table:
+          #   Substance_id_compound_id
           sid2cidData.append([aid, line[0], line[1]])
-      with open("%s/%s.csv" % (localProcessedDir, aid), 'w') as outf:
+
+      with open("%s/%s.csv" % (ungzippedFolder, aid), 'w') as outf:
         for line in assayData:                  
           outf.write(",".join(line)+"\n")
+
       with open(sid2cidMapFile, 'a') as outf: 
         for line in sid2cidData:
           outf.write(",".join(line)+"\n")
+    sys.stdout.write('\n')
      
+
 def loadMysqlTable(host, user, passwd, db):
   cnx = mysql.connector.connect(host=host, user=user, passwd=passwd, db=db, client_flags=[ClientFlag.LOCAL_FILES])
   cursor = cnx.cursor()
-  # Prepare table for insertion
-  print "> preparing mysql"
+
+  # Disable table keys and lock the Bioassasys table. This will speed up writes since
+  # we are making so many LOAD DATA LOCAL INFILE calls.
   try:
     cursor.execute("ALTER TABLE `Bioassays` DISABLE KEYS;");
     cursor.execute("LOCK TABLES `Bioassays` WRITE;")
   except mysql.connector.Error as e:
     sys.stderr.write("x failed preparing Bioassays: %s\n" % e)
     
-  # Execute insertions
-  print "> gathering filed to be inserted..."
-  root,_,files = next(os.walk(localProcessedDir))
+  root,_,files = next(os.walk(ungzippedFolder))
   for i in range(0, len(files)):
     sys.stdout.write("\r> loading files into table (%08d/%08d)" % (i+1, len(files)))
+    sys.stdout.flush()
     try:
       query = (
-          "LOAD DATA LOCAL INFILE '%s'"
-          " REPLACE"
-          " INTO TABLE Bioassays"
-          " FIELDS TERMINATED BY ','"
-          " LINES TERMINATED BY '\n' ("
-          "   assay_id,"
-          "   substance_id,"
-          "   activity_outcome,"
-          "   activity_score,"
-          "   activity_URL,"
-          "   assay_comment"
-          ");" % (os.path.join(root, files[i])))
+          "LOAD DATA LOCAL INFILE '%s' REPLACE "
+          "INTO TABLE Bioassays "
+          "FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' ("
+          " assay_id,"
+          " substance_id,"
+          " activity_outcome,"
+          " activity_score,"
+          " activity_URL,"
+          " assay_comment);" % (os.path.join(root, files[i])))
       cursor.execute(query)
     except mysql.connector.Error as e:
       sys.stderr.write("x failed loading data into Bioassays: %s\n" % e)
+  sys.stdout.write('\n')
 
-  # Commit changes, unlock the table and rebuild indexes
-  print "> rebuilding table indexes" 
+  # Unlock the tables and rebuild  indexes. This can also take a very long time
+  # to complete.
   try:
     cursor.execute("UNLOCK TABLES;")
     cursor.execute("ALTER TABLE `Bioassays` ENABLE KEYS;")
-    cnx.commit()
   except mysql.connector.Error as e:
     sys.stderr.write("x failed re-enabling keys on Bioassays: %s\n" % e)
+
   cursor.close()
   cnx.close()
+
 
 def update(user, passwd, db, host="127.0.0.1"):
   print "plugin: %s" % plugin
   print "> creating space on local machine"
-  makedirs([assayDataDir, localZippedDir, localUnzippedDir, 
-             localUngzippedDir, localProcessedDir, substanceDataDir])
+  makedirs([assayFolder, zippedFolder, unzippedFolder, 
+             ungzippedFolder, substanceFolder])
   print "> downloading updated files"
   downloadFiles()
   print "> unzipping files"
   unzipFiles()
   print "> begin splitting data into separate files"
-  splitDataFiles()
+  ungzipFiles()
   print "> loading data into table"
   loadMysqlTable(host, user, passwd, db)
   print "> %s complete\n" % plugin
