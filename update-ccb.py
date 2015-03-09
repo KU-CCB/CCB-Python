9fb29c34da97e96c6e82261bd88e4d6ede243251
kharland <kharland2011@gmail.com>
2015-03-09 13:44:50 -0500

    update-ccb.py

--- update-ccb.py
+++ update-ccb.py
@@ -15,7 +15,7 @@ import ConfigParser
 import plugins.Aid2GiGeneidAccessionUniprot
 import plugins.Assay_id_assay_description
 import plugins.Bioassays
-
+import plugins.Substance_id_compound_id
 
 cfg = ConfigParser.ConfigParser()
 cfg.read("config.cfg")
@@ -68,9 +68,10 @@ if hostname is None: hostname = "127.0.0.1"
 #
 # There are some files that can run in a different order, but it is best not to
 # change the order at all. 
-#plugins.Aid2GiGeneidAccessionUniprot.update(username, password, database, hostname);
-#plugins.Bioassays.update(username, password, database, hostname);
+plugins.Aid2GiGeneidAccessionUniprot.update(username, password, database, hostname);
+plugins.Bioassays.update(username, password, database, hostname);
 plugins.Assay_id_assay_description.update(username, password, database, hostname);
+plugins.Substance_id_compound_id.update(username, password, database, hostname);
 
 
 
