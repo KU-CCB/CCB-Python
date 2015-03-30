7747059bc5a130d5ab76bf40665354b77d78f5c1
Kendal Harland <kharland2011@gmail.com>
2015-03-30 13:38:32 -0500

    added logging support to Assays.py

--- plugins/Assays.py
+++ plugins/Assays.py
@@ -12,6 +12,8 @@ import mysql.connector
 from mysql.connector import errorcode
 from mysql.connector.constants import ClientFlag
 
+import logger
+
 __all__ = ["update"]
 plugin = __name__[__name__.index('.')+1:] if __name__ != "__main__"  else "main"
 cfg = ConfigParser.ConfigParser()
@@ -21,6 +23,7 @@ assayDescriptionFolder = "%s/assays/description" % cfg.get('default', 'tmp')
 OVERWRITE_EXISTING_FILES = False
 
 def makedirs(dirs):
+  logger.log("creating directories %s" % dirs)
   for d in dirs:
     if not os.path.exists(d):
       os.makedirs(d)
@@ -28,7 +31,8 @@ def makedirs(dirs):
 
 def downloadDescriptions(host, user, passwd, db):
   # Connect to mysql
-  cnx = mysql.connector.connect(host=host, user=user, passwd=passwd, db=db, client_flags=[ClientFlag.LOCAL_FILES])
+  cnx = mysql.connector.connect(host=host, user=user, passwd=passwd, db=db, 
+    client_flags=[ClientFlag.LOCAL_FILES])
   cursor = cnx.cursor()
   cursor.execute("SELECT DISTINCT(assay_id) FROM Bioassays;")
 
@@ -37,33 +41,28 @@ def downloadDescriptions(host, user, passwd, db):
 
   # Begin downloading all descriptions
   for i in range(0, len(aids)):
-    sys.stdout.write("\r> downloading description for assay (%08d/%08d)" %
-        (i+1, len(aids)))
-    sys.stdout.flush()
-    
+    logger.log("downloading description for assay (%08d/%08d) %d" %
+        (i+1, len(aids), aids[i]))
+
     # Skip existing files unless OVERWRITE_EXISTING FILES is True
     outFileName = "%s/%s.csv" % (assayDescriptionFolder, aids[i])
     if not os.path.exists(outFileName) or OVERWRITE_EXISTING_FILES:
       with open(outFileName, 'w') as outfile:
-        # Remove whitespace in response before writing the file
         response = pypug.getAssayDescriptionFromAID(aids[i]).encode('utf-8')
         if len(response) > 2:
           description = json.dumps(json.loads(response), separators=(',', ':'))
           outfile.write("%s %s" % (aids[i], description))
-  sys.stdout.write('\n')
-
 
 def loadMysqlTable(host, user, passwd, db):
-  cnx = mysql.connector.connect(host=host, user=user, passwd=passwd, db=db, 
-                                client_flags=[ClientFlag.LOCAL_FILES])
+  cnx = mysql.connector.connect(host=host, user=user, passwd=passwd, db=db,
+    client_flags=[ClientFlag.LOCAL_FILES])
   cursor = cnx.cursor()
 
   # Load each description file
   root,_,files = next(os.walk(assayDescriptionFolder))
   for i in range(0, len(files)):
-    sys.stdout.write("\r> loading files into table (%08d/%08d)" %
-      (i+1, len(files)))
-    sys.stdout.flush()
+    logger.log("loading files into table (%08d/%08d) %d" %
+      (i+1, len(files), aids[i]))
     try:
       query = ("LOAD DATA LOCAL INFILE '%s' REPLACE"
                "INTO TABLE Assay_id_assay_description"
@@ -72,15 +71,17 @@ def loadMysqlTable(host, user, passwd, db):
                "  assay_description);"
       cursor.execute(query, os.path.join(root, files[i]))
     except mysql.connector.Error as e:
-      sys.stderr.write("x failed loading data: %s\n" % e)
-  sys.stdout.write('\n')
+      logger.error("failed loading data: %s" % str(e))
 
 def update(user, passwd, db, host):
-  print "plugin: [%s]" % plugin
-  print "> creating space on local machine"
-  makedirs([assayDescriptionFolder])
-  print "> downloading assay descriptions"
-  downloadDescriptions(host, user, passwd, db)
-  print "> loading data into table"
-  loadMysqlTable(host, user, passwd, db)
-  print "> %s complete\n" % plugin
+  logger.log("beginning update")
+  try:
+    directories = [
+      assayDescriptionFolder]
+    makedirs(directories)
+    downloadDescriptions(host, user, passwd, db)
+    loadMysqlTable(host, user, passwd, db)
+    logger.log("update complete")
+  except Exception as e: # Any uncaught errors
+    sys.stderr.write(str(e))
+    logger.error(str(e))
