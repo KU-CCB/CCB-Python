112b9b56ebde077bc823aae9b77da398539dd8bf
Kendal Harland <kharland2011@gmail.com>
2015-05-25 09:46:08 -0500

    remove help() comments and an arbitrary assignment to 'hostname'

--- update_ccb.py
+++ update_ccb.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 
 import sys
+import subprocess
+import socket
+import getopt
 """
 mysq.connector is only installed on the acf cluster for python2.6 but we need
 to use python2.7+ because the 'with' directive is not backwards compatible
 with v2.6.*
 """
-import subprocess
-import socket
-import getopt
 if socket.gethostname()[-6:] == "ku.edu":
 	sys.path.append('/usr/lib/python2.6/site-packages/')
 import ConfigParser
@@ -21,22 +21,16 @@ cfg = ConfigParser.ConfigParser()
 cfg.read("config.cfg")
 
 def help():
-  usage = ""
-  print "---------------------------------------------------------"
-  print "%s - The %s database update script" % (__file__, cfg.get('default', 'database'))
-  print "---------------------------------------------------------"
-  print "For information, see %s." % (cfg.get('repo', 'url'))
-  print "Report issues at %s" % cfg.get('repo', 'issues')
-  print "contact the author %s <%s>" % (cfg.get('author', 'name'), cfg.get('author', 'email'))
   print ""
-  print "Usage: %s [-h|--hostname=127.0.0.1] [-u|--username] [-p|--password]" % __file__
+  print "Usage: python %s [-H|--hostname=127.0.0.1] [-u|--username] [-p|--password]" % __file__
   print ""
 
 # Read command line options
 shortargs = "hH:u:p:"
 longargs  = ["help","hostname=","username=","password="]
 opts, args = getopt.getopt(sys.argv[1:], shortargs, longargs)
-hostname, username, password, database = None,None,None,cfg.get('default','database')
+hostname, username, password, database = "127.0.0.1", None, None, cfg.get('default','database')
+
 if len(opts) == 0:
   help()
   sys.exit()
@@ -52,9 +46,6 @@ for option, value in opts:
     hostname = value
   else:
     assert False, "unhandled option"
-
-if hostname is None: hostname = "127.0.0.1"
-
 
 # Run table update scripts
 plugins.Substances.update(username, password, database, hostname);
